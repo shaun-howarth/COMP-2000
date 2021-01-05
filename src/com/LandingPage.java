491ee9a63f9594ef67f8aca8fbe3015c0eed28ae
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2021-01-05 17:45:26 +0000

    Have linked relevant forms to Jbuttons: LandingPage
    Linked Jbuttons on LandingPage to corresponding forms as indicated by button. Have so far added cursor hover effcet for two Jbuttons.

--- src/com/LandingPage.java
+++ src/com/LandingPage.java
@@ -17,9 +17,37 @@ public class LandingPage extends JFrame {
         setPreferredSize(new Dimension(800, 600));
         pack();
 
-        // Continue with Event listener
 
+        //Hover cursor effect for all Jbuttons
+
+        kioskBtn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        loginBtn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+        // Event listener for Kiosk interface button: to go to kiosk page
+
+        kioskBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                KioskPage s = new KioskPage();
+                s.setVisible(true);
+
+
+            }
+        });
+
+        // Event listener for Admin Login button: to go to Admin login page
+
+        loginBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                AdminPage s = new AdminPage();
+                s.setVisible(true);
+            }
+        });
     }
+
+
 
     public static void main(String[] args){
 
