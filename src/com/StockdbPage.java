12a7091ae2faeaab8ed427357b02edced9b8c822
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2021-01-05 12:33:44 +0000

    Creating GUI swing form "StockdbPage"
    Have started to create GUI form for the checkout application named "StockdbPage". Have extended JFrame components to GUI class. Have created a method within the GUI class that sets the content pane and have set window dimensions. Have created a main method wtihin the GUI class for "StockdbPage" that instantiates the object. Added Jitems to GUI form.

---
+++ src/com/StockdbPage.java
@@ -0,0 +1,27 @@
+package com;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class StockdbPage extends JFrame {
+    private JPanel mainPanel;
+    private JTextArea stockArea;
+    private JButton homeBtn;
+
+    public StockdbPage() {
+        setContentPane(mainPanel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(800, 600));
+        pack();
+
+    }
+
+    public static void main (String[] args) {
+
+        StockdbPage page = new StockdbPage();
+        page.setVisible(true);
+    }
+
+
+
+}
