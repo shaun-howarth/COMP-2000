f649a9858acf446e2bee7c9760003386d2e3d1bb
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2021-01-05 11:59:20 +0000

    Creating GUI swing form " ReceiptPage"
    Have started to create GUI form for the checkout application named "ReceiptPage". Have extended JFrame components to GUI class. Have created a method within the GUI class that sets the content pane and have set window dimensions. Have created a main method wtihin the GUI class for "ReceiptPage" that instantiates the object. Have added required Jitems.

---
+++ src/com/ReceiptPage.java
@@ -0,0 +1,26 @@
+package com;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ReceiptPage extends JFrame {
+    private JPanel mainPanel;
+    private JTextArea listtextArea;
+    private JButton homeBtn;
+
+    public ReceiptPage() {
+        setContentPane(mainPanel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(800, 600));
+        pack();
+
+    }
+
+    public static void main (String[] args) {
+
+        ReceiptPage page = new ReceiptPage();
+        page.setVisible(true);
+    }
+
+
+}
