89cd6866416ba34467eb51e9a6756ec916007adc
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2021-01-05 12:20:15 +0000

    Creating GUI swing form "Creating GUI swing form "AdminPage""
    Have started to create GUI form for the checkout application named "AdminPage". Have extended JFrame components to GUI class. Have created a method within the GUI class that sets the content pane and have set window dimensions. Have created a main method wtihin the GUI class for "AdminPage" that instantiates the object. Added Jitems to GUI form.

---
+++ src/com/AdminPage.java
@@ -0,0 +1,30 @@
+package com;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AdminPage extends JFrame {
+    private JPanel mainPanel;
+    private JButton loginBtn;
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private JButton homeBtn;
+
+
+    public AdminPage() {
+        setContentPane(mainPanel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(800, 600));
+        pack();
+
+    }
+
+    public static void main (String[] args) {
+
+        AdminPage page = new AdminPage();
+        page.setVisible(true);
+    }
+
+
+
+}
