e5934edaf5fa8b32df5fb1bd924a6ea265d2a6ab
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2021-01-06 17:41:23 +0000

    Have linked relevant forms to Jbuttons: PaymentPage
    Linked Jbuttons on PaymentPage to corresponding forms as indicated by buttons. Have so far added cursor hover effcet for the Jbuttons.

--- src/com/PaymentPage.java
+++ src/com/PaymentPage.java
@@ -2,6 +2,8 @@ package com;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PaymentPage extends JFrame {
     private JPanel mainPanel;
@@ -16,6 +18,31 @@ public class PaymentPage extends JFrame {
         setPreferredSize(new Dimension(800, 600));
         pack();
 
+
+        //Hover cursor effect for all Jbuttons
+
+        cashBtn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        cardBtn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+        // Event listener for cash payment button: goes to CashPage with price amount
+
+        cashBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CashPage s = new CashPage();
+                s.setVisible(true);
+            }
+        });
+
+        // Event listener for card payment button: goes to CardPage with price amount
+
+        cardBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardPage s = new CardPage();
+                s.setVisible(true);
+            }
+        });
     }
 
 
