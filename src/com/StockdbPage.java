654458674151e12e2149a03831ea512bb118a31b
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2021-01-07 12:02:36 +0000

    Have linked relevant forms to Jbuttons: StockdbPage
    Linked Jbuttons on StockPage to corresponding forms as indicated by buttons. Have so far added cursor hover effcet for the Jbuttons.

--- src/com/StockdbPage.java
+++ src/com/StockdbPage.java
@@ -2,6 +2,8 @@ package com;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class StockdbPage extends JFrame {
     private JPanel mainPanel;
@@ -16,6 +18,20 @@ public class StockdbPage extends JFrame {
         setPreferredSize(new Dimension(800, 600));
         pack();
 
+        //Hover cursor effect for all Jbuttons
+
+        homeBtn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+
+        // Event listener for home button: will go to Landing Page
+
+        homeBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                LandingPage s = new LandingPage();
+                s.setVisible(true);
+            }
+        });
     }
 
     public static void main (String[] args) {
