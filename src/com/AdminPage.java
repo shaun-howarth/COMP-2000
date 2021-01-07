035ee18d76305badd2ef282fdf25ba37b971332a
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2021-01-07 11:28:14 +0000

    Have linked relevant forms to Jbuttons: AdminPage
    Linked Jbuttons on AdminPage to corresponding forms as indicated by buttons. Have so far added cursor hover effcet for the Jbuttons. Have linked additional actions to Jbutton (s).

--- src/com/AdminPage.java
+++ src/com/AdminPage.java
@@ -2,6 +2,8 @@ package com;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class AdminPage extends JFrame {
     private JPanel mainPanel;
@@ -19,7 +21,38 @@ public class AdminPage extends JFrame {
         setPreferredSize(new Dimension(800, 600));
         pack();
 
+
+        //Hover cursor effect for all Jbuttons
+
+        homeBtn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        loginBtn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+
+        // Event listener for home button: will go to landing page
+
+        homeBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                LandingPage s = new LandingPage();
+                s.setVisible(true);
+            }
+        });
+
+        // Event listener for admin login button: will go to stock DB
+        // Only if login is successful
+
+        loginBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                StockdbPage s = new StockdbPage();
+                s.setVisible(true);
+            }
+        });
     }
+
+
+
+
 
     public static void main (String[] args) {
 
